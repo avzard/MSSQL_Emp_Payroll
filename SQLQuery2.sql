107775ef413df5fd42f2ec299a600e726cc8ac50
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-18 14:00:25 +0530

    [Abhishek]Created Database

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+---Welcome To Employee Pay Roll Data Base Problem---
+--------------UC_1--------------
+--Create DataBase--
+create database payroll_service;
+--Switch To Working Data Base-- 
+use payroll_service;
